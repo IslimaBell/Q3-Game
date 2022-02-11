4d7b4dd3ffe65065335baca5a96ffb4ac9465fdc
MUKILTEO\1709546 <Isaiah2124.ib@gmail.com>
2022-02-11 11:46:07 -0800

    More art

---
+++ Assets/Isaiah/Scripts/DetectClimable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectClimable : MonoBehaviour
+{
+    [SerializeField]
+    private RefinedMovement player;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<VineScript>())
+        {
+            player.ClimbingAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<VineScript>())
+        {
+            player.ClimbingAllowed = false;
+        }
+    }
+
+}
